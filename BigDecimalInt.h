1a4ab49177db874012e9e08851538aa4b1727082
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2022-10-05 23:38:24 +0200

    Update BigDecimalInt.h

--- BigDecimalInt.h
+++ BigDecimalInt.h
@@ -1,7 +1,5 @@
-/*
 #ifndef ASSIGNMENT_1_BIGDECIMALINT_H
 #define ASSIGNMENT_1_BIGDECIMALINT_H
-*/
 
 #include <iostream>
 #include<deque>
@@ -10,27 +8,26 @@ using namespace std;
 
 class BigDecimalInt {
     friend ostream& operator<<(ostream& out,const BigDecimalInt& bigint);
-    public:
-        BigDecimalInt(const string& num);
-        BigDecimalInt(const int& num=0);
-        BigDecimalInt(const BigDecimalInt& num);
+public:
+    BigDecimalInt(const string& num);
+    BigDecimalInt(const int& num=0);
+    BigDecimalInt(const BigDecimalInt& num);
 
-        int getSize()const;
-        char getSign()const;
+    int getSize()const;
+    char getSign()const;
 
-        BigDecimalInt& operator=(const BigDecimalInt& num);
-        BigDecimalInt operator+(const BigDecimalInt& num)const;
-        BigDecimalInt operator-(const BigDecimalInt& num)const;
-        bool operator< (const BigDecimalInt& anotherDec)const;
-        bool operator> (const BigDecimalInt& anotherDec)const;
-        bool operator==(const BigDecimalInt& anotherDec)const;
+    BigDecimalInt& operator=(const BigDecimalInt& num);
+    BigDecimalInt operator+(const BigDecimalInt& num)const;
+    BigDecimalInt operator-(const BigDecimalInt& num)const;
+    bool operator< (const BigDecimalInt& anotherDec)const;
+    bool operator> (const BigDecimalInt& anotherDec)const;
+    bool operator==(const BigDecimalInt& anotherDec)const;
 
-    private:
-        deque<char> digits;
-        char sign;
-        void setSign(char sign);
+private:
+    deque<char> digits;
+    char sign;
+    void setSign(const char &sign);
 };
 
-/*
+
 #endif //ASSIGNMENT_1_BIGDECIMALINT_H
-*/
