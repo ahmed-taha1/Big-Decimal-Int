97aca70fa674c951c8998ab45509a69c6d2a617d
Ahmed Taha <101575320+ahmed-taha1@users.noreply.github.com>
2022-10-08 00:04:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,19 +3,49 @@
 using namespace std;
 
 int main(){
+    start:
+
+    // inputs window
     system("CLS");
+    cout << "please enter the first number : ";
     string s1;   cin >> s1;
-    BigDecimalInt h1(s1);
+    BigDecimalInt num1(s1);
 
+    cout << "please enter the second number : ";
     string s2;  cin >> s2;
-    BigDecimalInt h2(s2);
-    cout << h2 << " " << h2.getSign() << " " << h2.getSize() << '\n';
-    cout << h1 << " " << h1.getSign() << " " << h1.getSize() << '\n';
+    BigDecimalInt num2(s2);
 
-    BigDecimalInt h3 = h1 - h2;
-    cout << h3.getSign();
-    cout << BigDecimalInt(h1-h2);
+
+    // calculations' window
+    system("CLS");
+    cout << "num 1 : " << num1 << "\n";
+    cout << "num 2 : " << num2 << "\n";
+
+    BigDecimalInt num3 = num1 + num2;
+    cout << "num 1 + num 2 = " << num3 << '\n';
+
+    BigDecimalInt num4 = num1 - num2;
+    cout << "num1 - num 2 = " << num4 << '\n';
+
+    if(num1 == num2)
+        cout << "num1 = num2\n";
+    else if(num1 > num2)
+        cout << "num1 > num2\n";
+    else
+        cout << "num 1 < num2\n";
+    
+    // repeat window
+    choose:
+    cout << "do you want to exit ? (y/n) : ";
+    char c;     cin >> c;
+
+    if(c == 'y')
+        exit(0);
+    else if(c == 'n')
+        goto start;
+    else{
+        cout << "invalid input please try again!\n";
+        goto choose;
+    }
+        
 }
-
-// -123456789012345678901234567890
-// -113456789011345678901134567890
