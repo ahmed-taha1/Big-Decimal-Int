d5fc4ffd67b44208daec86db576f0eda2a0cb21b
Ahmed Taha <101575320+ahmed-taha1@users.noreply.github.com>
2022-10-26 23:50:40 +0200

    last update

--- BigDecimalInt.h
+++ BigDecimalInt.h
@@ -1,35 +1,33 @@
-#ifndef ASSIGNMENT_1_BIGDECIMALINT_H
-#define ASSIGNMENT_1_BIGDECIMALINT_H
-
-//#include <bits/stdc++.h>
-#include <iostream>
-#include<deque>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+
 class BigDecimalInt {
+    // cout operator overloading function
     friend ostream& operator<<(ostream& out,const BigDecimalInt& bigint);
 public:
-    BigDecimalInt(const string& num); // 
+    // constructors
+    BigDecimalInt(const string& num);
     BigDecimalInt(const int& num=0);
     BigDecimalInt(const BigDecimalInt& num);
 
+    // getter functions
     int getSize()const;
     char getSign()const;
 
+    // operator overloading functions
     BigDecimalInt& operator=(const BigDecimalInt& num);
     BigDecimalInt operator+(const BigDecimalInt& num)const;
     BigDecimalInt operator-(const BigDecimalInt& num)const;
     bool operator< (const BigDecimalInt& anotherDec)const;
     bool operator> (const BigDecimalInt& anotherDec)const;
     bool operator==(const BigDecimalInt& anotherDec)const;
+
 private:
-    deque<char> digits;
-    char sign;
+    deque<char> digits; // for storing digits from the string
+    char sign;  // to store the sign
+
+    // private functions
     void setSign(const char &sign);
     void validate(const string &num); // helper /utility function
-
 };
-
-
-#endif //ASSIGNMENT_1_BIGDECIMALINT_H
